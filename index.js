465ada465fe160aaca387fa7d54a4105b4884dc2
abolfazl13a <98390584+abolfazl13a@users.noreply.github.com>
2022-01-25 15:29:01 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const botconfig = require(`./botconfig.json`);
+const colors = require(`./colors.json`);
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ready`);
+});
+
+client.on('guildMemberAdd', member => {
+    let welcomeChannel = client.channels.cache.get("935466733718867998") //inja vared kon
+    let targetchannel = 'Channel_ID' //inja vared kon
+    if(welcomeChannel){
+
+        let welcomeEmbed = new discord.MessageEmbed()
+
+        if(member.user.bot){
+
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username} (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }else{
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی   \امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username}  (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }
+    }else{
+        console.log("Welcome Channel Yaft Nashod")
+    }
+})
+
+
+client.login(botconfig.token);
+
