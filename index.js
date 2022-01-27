394c141575247025a563d6165d2772dde74d7914
abolfazl13a <98390584+abolfazl13a@users.noreply.github.com>
2022-01-27 13:28:23 +0330

    indexx
    discordd

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const Discord = require('discord.js');
-const client = new Discord.client();
-const config = require
+const botconfig = require(`./botconfig.json`);
+const colors = require(`./colors.json`);
+const discord = require('discord.js');
+const client = new discord.Client();
 
 client.on('ready', () => {
-    console.log('Online Shod');
-        client.user.setstatus("dnd");
-        let targetguild = client.guilds.cache.get
-        ("790312889990119424")
-        setInterval(function(){
-            client.user.setActivity('$
-            {targetguild.memberCount} members' , {type:
-            "COMPETING" })
-    },)   
-    
-    })
-client.login(process.env.token)    
+    console.log(`${client.user.tag} is ready`);
+});
+
+client.on('guildMemberAdd', member => {
+    let welcomeChannel = client.channels.cache.get("935466733718867998") //inja vared kon
+    let targetchannel = 'Channel_ID' //inja vared kon
+    if(welcomeChannel){
+
+        let welcomeEmbed = new discord.MessageEmbed()
+
+        if(member.user.bot){
+
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username} (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }else{
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی   \امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username}  (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }
+    }else{
+        console.log("Welcome Channel Yaft Nashod")
+    }
+})
+
+
+client.login(botconfig.token);
+
